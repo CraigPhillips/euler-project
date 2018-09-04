8f903ee665e8c0cf79423bea182b4927553d4c24
Craig Phillips <craig.phillips@frozenexports.net>
2018-09-04 23:16:02 +0000

    adds some performance analysis comments

--- src/solutions/00001/index.mjs
+++ src/solutions/00001/index.mjs
@@ -1,8 +1,11 @@
+// O(values.length * cap)
 export function getMultiplesUnderCap(values, cap) {
   const multiples = {};
 
+  // O(values.length)
   values.forEach((value) => {
     let nextMultiple = value;
+    // O(cap)
     while (nextMultiple < cap) {
       if (!multiples[nextMultiple]) multiples[nextMultiple] = nextMultiple;
       nextMultiple += value;
@@ -12,10 +15,15 @@ export function getMultiplesUnderCap(values, cap) {
   return multiples;
 }
 
+// O(values.length * cap) although it seems like values will usually be
+// pretty low and in that case, this becomes linear with respect to the
+// provided cap
 export function sumMultiplesUnderCap(values, cap) {
+  // O(values.length * cap)
   const multiples = getMultiplesUnderCap(values, cap);
   return Object
     .values(multiples)
+    // O(cap)
     .reduce((soFar, nextVal) => soFar + nextVal, 0);
 }
 
