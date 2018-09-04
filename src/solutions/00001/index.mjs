f23a630e72f87d3b8f0d711ccd7700b23692e131
Craig Phillips <craig.phillips@frozenexports.net>
2018-09-04 23:07:07 +0000

    adds raw solution to problem #1

--- src/solutions/00001/index.mjs
+++ src/solutions/00001/index.mjs
@@ -1,3 +1,24 @@
+export function getMultiplesUnderCap(values, cap) {
+  const multiples = {};
+
+  values.forEach((value) => {
+    let nextMultiple = value;
+    while (nextMultiple < cap) {
+      if (!multiples[nextMultiple]) multiples[nextMultiple] = nextMultiple;
+      nextMultiple += value;
+    }
+  });
+
+  return multiples;
+}
+
+export function sumMultiplesUnderCap(values, cap) {
+  const multiples = getMultiplesUnderCap(values, cap);
+  return Object
+    .values(multiples)
+    .reduce((soFar, nextVal) => soFar + nextVal, 0);
+}
+
 export default function solve() {
-  console.log('solving problem 1...');
+  console.log(sumMultiplesUnderCap([3, 5], 1000));
 }
