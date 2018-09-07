32ba7321492131db9018ddfff028daef0403efc4
Craig Phillips <Craig.Phillips@FrozenExports.net>
2018-09-07 14:20:27 -0700

    adds solution to problem 3 and removes some notation I had left in solution 1

---
+++ src/solutions/00003/index.mjs
@@ -0,0 +1,34 @@
+export function getPrimeFactors(num) {
+  if (!num || typeof num !== 'number' || num < 1 || Math.floor(num) !== num) {
+    throw new Error(`${num} is not a positive integer`);
+  }
+
+  const primesOtherThan1 = [];
+  const primeFactors = [1];
+  let remainingToFactor = num;
+
+  for (let i = 2; i <= remainingToFactor; i += 1) {
+    const isPrime = !primesOtherThan1.some(prime => i % prime === 0);
+    if (isPrime) {
+      primesOtherThan1.push(i);
+
+      if (remainingToFactor % i === 0) {
+        while (remainingToFactor % i === 0) {
+          remainingToFactor /= i;
+        }
+
+        primeFactors.push(i);
+      }
+    }
+  }
+  if (remainingToFactor !== 1) primeFactors.push(remainingToFactor);
+
+  return primeFactors;
+}
+
+export default function solve() {
+  const target = 600851475143;
+
+  const primesFactors = getPrimeFactors(target);
+  return primesFactors[primesFactors.length - 1];
+}
--- src/solutions/index.mjs
+++ src/solutions/index.mjs
@@ -1,7 +1,9 @@
 import problem00001 from './00001';
 import problem00002 from './00002';
+import problem00003 from './00003';
 
 export default {
   problem00001,
   problem00002,
+  problem00003,
 };
