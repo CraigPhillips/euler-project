ef4aa08c0d6f38415881513623eb55deb200312f
Craig Phillips <Craig.Phillips@FrozenExports.net>
2018-09-10 11:55:33 -0700

    adds solution for problem 7

--- src/solutions/00007/index.mjs
+++ src/solutions/00007/index.mjs
@@ -1,5 +1,14 @@
-export const answer = -1;
+export const answer = 104743;
+
+export function getPrimes(n) {
+  const primes = [];
+  const isPrime = num => !primes.some(toTest => num % toTest === 0);
+  for (let i = 2; primes.length < n; i += 1) if (isPrime(i)) primes.push(i);
+  return primes;
+}
 
 export function solve() {
-  return -1;
+  const tenThousandFirst = 10001;
+  const primes = getPrimes(tenThousandFirst);
+  return primes[primes.length - 1];
 }
