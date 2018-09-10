c792735a6a6b547ff28c751940d09bbb2c0afe13
Craig Phillips <Craig.Phillips@FrozenExports.net>
2018-09-09 21:24:55 -0700

    adds solution for problem 5

--- src/solutions/00005/index.mjs
+++ src/solutions/00005/index.mjs
@@ -1,4 +1,19 @@
-export const answer = -1;
+import getAllPrimeFactors from '../../factoring/getAllPrimeFactors';
+
+export const answer = 232792560;
+
+export function getSmallestProductOfPrimeFactors(factors) {
+  return Object.entries(factors).reduce(
+    (soFar, [factor, count]) => soFar * (parseInt(factor, 10) ** count),
+    1,
+  );
+}
 
 export function solve() {
+  const numbersThroughTwenty = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
+
+  const primeFactors = getAllPrimeFactors(numbersThroughTwenty);
+  return getSmallestProductOfPrimeFactors(primeFactors);
 }
--- src/solutions/index.mjs
+++ src/solutions/index.mjs
@@ -2,10 +2,12 @@ import * as problem00001 from './00001';
 import * as problem00002 from './00002';
 import * as problem00003 from './00003';
 import * as problem00004 from './00004';
+import * as problem00005 from './00005';
 
 export default {
   problem00001,
   problem00002,
   problem00003,
   problem00004,
+  problem00005,
 };
