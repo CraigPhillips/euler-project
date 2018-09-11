962ad18838712883a2ac490db7cc6774a5e9d2a6
Craig Phillips <Craig.Phillips@FrozenExports.net>
2018-09-11 16:01:07 -0700

    refactors get N primes method to a reusable module

--- src/solutions/00007/index.mjs
+++ src/solutions/00007/index.mjs
@@ -1,14 +1,6 @@
-export const answer = 104743;
+import getPrimes from '../../primes/getPrimes';
 
-// this is essentially using the Sieve of Eratosthenes to find primes which
-// is not as efficient as wheel sieves or the sieve of Atkin but I didn't take
-// the time to get into those
-export function getPrimes(n) {
-  const primes = [];
-  const isPrime = num => !primes.some(toTest => num % toTest === 0);
-  for (let i = 2; primes.length < n; i += 1) if (isPrime(i)) primes.push(i);
-  return primes;
-}
+export const answer = 104743;
 
 export function solve() {
   const tenThousandFirst = 10001;
