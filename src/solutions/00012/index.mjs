d2dbb64987639c919538136bc082656d4a8c488d
Craig Phillips <craig.phillips@frozenexports.net>
2018-09-12 22:14:35 +0000

    adds skeleton of solution for problem 12

---
+++ src/solutions/00012/index.mjs
@@ -0,0 +1,10 @@
+export const answer = -1;
+
+export function getFirstTriangularNumberWithFactorCountOver(n) {
+  return - 1;
+}
+
+export function solve() {
+  const fiveHundredFactors = 500;
+  return getFirstTriangularNumberWithFactorCountOver(fiveHundredFactors);
+}
