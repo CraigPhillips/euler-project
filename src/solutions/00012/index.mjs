3a2dfc9c4a26b8f641e1dd4694cd5f0d90278243
Craig Phillips <Craig.Phillips@FrozenExports.net>
2018-09-13 16:03:34 -0700

    adds a solution for problem 12 but it seems to run really slowly in Jest, which I am going to try to improve

--- src/solutions/00012/index.mjs
+++ src/solutions/00012/index.mjs
@@ -1,10 +1,34 @@
-export const answer = -1;
+export const answer = 76576500;
+
+export function getAllFactors(n) {
+  const factors = {};
+
+  for (let i = 1; i <= Math.ceil(Math.sqrt(n)); i += 1) {
+    if (n % i === 0) {
+      factors[i] = true;
+      factors[n / i] = true;
+    }
+  }
+
+  return Object.keys(factors);
+}
 
 export function getFirstTriangularNumberWithFactorCountOver(n) {
-  return - 1;
+  let nextTriangular = 0;
+  let triangularCount = 0;
+  let factors = [];
+
+  while (factors.length <= n) {
+    triangularCount += 1;
+    nextTriangular += triangularCount;
+    factors = getAllFactors(nextTriangular);
+  }
+
+  return { factors, number: nextTriangular, triangularCount };
 }
 
 export function solve() {
   const fiveHundredFactors = 500;
-  return getFirstTriangularNumberWithFactorCountOver(fiveHundredFactors);
+  return getFirstTriangularNumberWithFactorCountOver(fiveHundredFactors)
+    .number;
 }
