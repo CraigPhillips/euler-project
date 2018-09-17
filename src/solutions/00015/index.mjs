cba314f37f1543ad321f4de7397743315a86fe3c
Craig Phillips <Craig.Phillips@FrozenExports.net>
2018-09-17 15:54:01 -0700

    adds start of iterative solution to problem 15 but needs some refactoring and testing

--- src/solutions/00015/index.mjs
+++ src/solutions/00015/index.mjs
@@ -27,7 +27,31 @@ export function solve() {
   // considers each corner to be a vertex so there is one more row and column
   // of vertices than there are squares in the graph
   const graph = buildLatticeGraph(twentyByTwentySquaresInGrid + 1);
-  console.log(graph);
+  let nextNodesToProcess = [graph.bottomRightNode];
+  graph.bottomRightNode.value = 1;
+
+  while (nextNodesToProcess.length > 0) {
+    const theseNodes = nextNodesToProcess;
+    nextNodesToProcess = [];
+
+    for (let i = 0; i < theseNodes.length; i += 1) {
+      theseNodes[i].value = theseNodes[i]
+        .edges
+        .filter(edge => edge.destination.value !== undefined)
+        .reduce((sum, edge) => sum + edge.destination.value, 0);
+    }
+
+    for (let i = 0; i < theseNodes.length; i += 1) {
+      theseNodes[i]
+        .edges
+        .filter(edge => edge.destination.value === undefined)
+        .forEach((edge) => {
+          if (nextNodesToProcess.indexOf(edge.destination) === -1) {
+            nextNodesToProcess.push(edge.destination)
+          }
+        });
+    }
+  }
 
   return -1;
 }
