0b6024ef449eae1a5c1a4b7374172374f5fb9405
Craig Phillips <craig.phillips@frozenexports.net>
2018-09-18 22:49:58 +0000

    adds solution for problem 16 and expanded testing output to make slowdowns more apparent

--- src/solutions/00016/index.mjs
+++ src/solutions/00016/index.mjs
@@ -1,7 +1,25 @@
-import { multiplyStrings } from '../../string-math/multiplyStrings';
+import { multiplyStrings } from '../../string-math';
 
-export const answer = -1;
+export const answer = 1366;
+
+export function addDigitsOfPowResult(num, pow) {
+  if (pow < 0) throw new Error('can not handle powers less than 0');
+  if (pow === 0) return 1;
+  const stringified = num.toString();
+
+  let powResult = stringified;
+  for (let i = 1; i < pow; i += 1) {
+    powResult = multiplyStrings(powResult, stringified);
+  }
+
+  return powResult
+    .split('')
+    .reduce((soFar, next) => soFar + parseInt(next, 10), 0);
+}
 
 export function solve() {
-  return -1;
+  const two = 2;
+  const powerOfDigitSumWanted = 1000;
+
+  return addDigitsOfPowResult(two, powerOfDigitSumWanted);
 }
