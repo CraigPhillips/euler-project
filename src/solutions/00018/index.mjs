fd0f579197d13e450650727a137765ec302473e2
Craig Phillips <Craig.Phillips@FrozenExports.net>
2018-09-19 11:33:18 -0700

    adds solution for problem 18

--- src/solutions/00018/index.mjs
+++ src/solutions/00018/index.mjs
@@ -1,8 +1,8 @@
-import { example } from './triangles';
+import { problem } from './triangles';
 
-export const answer = -1;
+export const answer = 1074;
 
-export function findMaxTrianglePath(triangle) {
+export function findAMaxTrianglePath(triangle) {
   const maxTri = [];
 
   while (maxTri.length < triangle.length) {
@@ -11,15 +11,29 @@ export function findMaxTrianglePath(triangle) {
     for (let i = 0; i < triangle[nextTriI].length; i += 1) {
       const value = triangle[nextTriI][i];
       if (nextTriI === 0) nextMax.push({ path: `${value} (${i})`, value });
+      else {
+        const prev = maxTri[nextTriI - 1];
+        const useLeft = prev[i - 1]
+          && (!prev[i] || prev[i - 1].value > prev[i].value);
+        const nodeToUse = useLeft ? prev[i - 1] : prev[i];
+        nextMax.push({
+          path: `${nodeToUse.path} -> ${value} (${i})`,
+          value: value + nodeToUse.value,
+        });
+      }
     }
     maxTri.push(nextMax);
   }
 
-  console.log(maxTri);
+  let result = { value: 0 };
+  const lastMaxRow = maxTri[maxTri.length - 1];
+  for (let i = 0; i < lastMaxRow.length; i += 1) {
+    if (lastMaxRow[i].value > result.value) result = lastMaxRow[i];
+  }
 
-  return {};
+  return result;
 }
 
 export function solve() {
-  return findMaxTrianglePath(example);
+  return findAMaxTrianglePath(problem).value;
 }
