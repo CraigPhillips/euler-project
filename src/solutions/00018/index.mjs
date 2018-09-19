6db13c0ca8f4735e9dab1e30fe56ae9c116ad8f3
Craig Phillips <craig.phillips@frozenexports.net>
2018-09-19 17:33:50 +0000

    adds base case for dynamic programming solution to finding a max triangle path

--- src/solutions/00018/index.mjs
+++ src/solutions/00018/index.mjs
@@ -2,8 +2,24 @@ import { example } from './triangles';
 
 export const answer = -1;
 
-export function solve() {
-  console.log(example);
+export function findMaxTrianglePath(triangle) {
+  const maxTri = [];
 
-  return -1;
+  while (maxTri.length < triangle.length) {
+    const nextTriI = maxTri.length;
+    const nextMax = [];
+    for (let i = 0; i < triangle[nextTriI].length; i += 1) {
+      const value = triangle[nextTriI][i];
+      if (nextTriI === 0) nextMax.push({ path: `${value} (${i})`, value });
+    }
+    maxTri.push(nextMax);
+  }
+
+  console.log(maxTri);
+
+  return {};
+}
+
+export function solve() {
+  return findMaxTrianglePath(example);
 }
