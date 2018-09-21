a2d2846133f6e7a6e37be2ec19ebe526c4b453a8
Craig Phillips <Craig.Phillips@FrozenExports.net>
2018-09-21 09:58:59 -0700

    adds solution for problem 20

--- src/solutions/00020/index.mjs
+++ src/solutions/00020/index.mjs
@@ -1,5 +1,22 @@
-export const answer = -1;
+import { multiplyStrings } from '../../string-math';
+
+export const answer = 648;
+
+export function sumFactorialDigits(n) {
+  let stringifiedFactorial = '1';
+  let factorialDigitSum = 0;
+  for (let i = 2; i <= n; i += 1) {
+    stringifiedFactorial = multiplyStrings(stringifiedFactorial, i.toString());
+  }
+  for (let i = 0; i < stringifiedFactorial.length; i += 1) {
+    factorialDigitSum += parseInt(stringifiedFactorial[i], 10);
+  }
+
+  return { factorialDigitSum, stringifiedFactorial };
+}
 
 export function solve() {
-  return -1;
+  const oneHundred = 100;
+
+  return sumFactorialDigits(oneHundred).factorialDigitSum;
 }
