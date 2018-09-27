c9332edd7d44446546879dc3f74ac54dd1c2c82a
Craig Phillips <Craig.Phillips@FrozenExports.net>
2018-09-27 08:58:21 -0700

    renames some interal variable names to accurately refer to them as permutations rather than combinations

--- src/combinatorics/getAllPermutations.mjs
+++ src/combinatorics/getAllPermutations.mjs
@@ -4,22 +4,22 @@ function getAllPermutationsOfLengthFrom(
   startI = 0,
 ) {
   if (length === 0) return [[]];
-  const combinations = [];
+  const permutations = [];
 
   for (let i = startI; i < set.length - (length - 1); i += 1) {
     const subSets = getAllPermutationsOfLengthFrom(set, length - 1, i + 1);
     for (let j = 0; j < subSets.length; j += 1) {
       const thisSubSet = subSets[j];
-      combinations.push([set[i]].concat(thisSubSet));
+      permutations.push([set[i]].concat(thisSubSet));
     }
   }
-  return combinations;
+  return permutations;
 }
 
 export default function getAllPermutations(set) {
-  let combinations = [];
+  let permutations = [];
   for (let i = 0; i <= set.length; i += 1) {
-    combinations = combinations.concat(getAllPermutationsOfLengthFrom(set, i));
+    permutations = permutations.concat(getAllPermutationsOfLengthFrom(set, i));
   }
-  return combinations;
+  return permutations;
 }
