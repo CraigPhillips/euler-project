cabc12d9f3ab0dc9ed00a18e02fdb2640c7a531b
Craig Phillips <Craig.Phillips@FrozenExports.net>
2018-09-30 13:51:45 -0700

    adds solution for problem 24

--- src/solutions/00001-000100/00021-00030/00024/index.mjs
+++ src/solutions/00001-000100/00021-00030/00024/index.mjs
@@ -1,5 +1,14 @@
-export const answer = -1;
+import getNext from '../../../../combinatorics/getNextLexographicPermutation';
+
+export const answer = 2783915460;
 
 export function solve() {
-  return -1;
+  let nextPermutation = '0123456789';
+  const millionthPermutation = 1000000;
+
+  for (let i = 1; i < millionthPermutation; i += 1) {
+    nextPermutation = getNext(nextPermutation);
+  }
+
+  return parseInt(nextPermutation, 10);
 }
