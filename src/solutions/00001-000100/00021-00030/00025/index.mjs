779613ec55066d73d653d4c35b4e80b69b6dce91
Craig Phillips <craig.phillips@frozenexports.net>
2018-10-01 17:10:09 +0000

    add brute force solution for problem 25 - it is fast enough using the string adding library

--- src/solutions/00001-000100/00021-00030/00025/index.mjs
+++ src/solutions/00001-000100/00021-00030/00025/index.mjs
@@ -1,5 +1,24 @@
-export const answer = -1;
+import { addStrings } from '../../../../string-math';
+
+export const answer = 4782;
+
+export function getFirstFibWithDigits(digits) {
+  let fibNMinus2 = '1';
+  let fibNMinus1 = '1';
+  let nextFib = '2';
+  let nextI = 3;
+
+  while (nextFib.length < digits) {
+    nextI += 1;
+    fibNMinus2 = fibNMinus1;
+    fibNMinus1 = nextFib;
+    nextFib = addStrings(fibNMinus2, fibNMinus1);
+  }
+
+  return { fibI: nextI, fib: nextFib };
+}
 
 export function solve() {
-  return -1;
+  const oneThousandDigits = 1000;
+  return getFirstFibWithDigits(oneThousandDigits).fibI;
 }
